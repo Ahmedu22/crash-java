2da73d27a68fdfc1d3d4c73056d7d9e51662d26a
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-06 17:42:54 +0300

    crash-java

---
+++ index.js
@@ -0,0 +1,30 @@
+alert(7)
+
+// variables
+// keywords = var, let, const
+
+var x = 10
+console.log(x)
+ 
+let name = "Ahmed";
+let lastname ="Yussuf";
+console.log(name + " " + lastname)
+
+const age = "32";
+console.log(age)
+
+
+
+// Rules
+
+// * contains only letters, numbers, _ , $ sign
+// * cannot start with numbers
+
+// naming variables
+// 1.camel case
+// const firstName = "Mohamed"
+// 2.underscore method
+// const first-name = "Ahmed"
+// 3.pascalcase method
+// const LastName = "Ahmed"
+
