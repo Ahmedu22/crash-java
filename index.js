ada4602e2fea3df9b4133da4e1fa67af9d501ab8
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-08 17:50:41 +0300

    crash-java

--- index.js
+++ index.js
@@ -28,3 +28,47 @@ console.log(age)
 // 3.pascalcase method
 // const LastName = "Ahmed"
 
+// two tpyes of data types
+// 1.primitive
+
+// .numbers
+// let age = "22";
+// console.log(typeof age);
+
+// .strings
+// const name = "Ahmed";
+// console.log(typeof name)
+
+// .symbol
+
+// .boolean
+const isPlaying = false;
+console.log(typeof isPlaying);
+
+// .null/undefined
+const count = null;
+console.log(count)
+
+let car;
+console.log(car)
+
+// 2.object
+
+// .Arrays  : index wa godka elemet wa waxa kugudajira
+// array litterals []
+let list = [4,5,7,8];
+list[4] = 'yes'
+console.log(list)
+
+// array constructer array()
+let numbers = new Array(2,3,4)
+numbers[3] = 'ahmed'
+console.log(numbers)
+
+
+// Array.of(), Array.from()
+
+// .function
+
+// .objects
+// .anthing else which is not primitive
